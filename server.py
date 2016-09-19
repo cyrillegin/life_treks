fb5880eb05efafb8a65bc423812ee4d3c72ecdb7
Cyrille Gindreau <Cyrille@Cyrilles-MacBook-Pro.local>
2016-09-19 16:32:26 -0600

    first commit

---
+++ server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import os
+import cherrypy
+
+
+PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+
+
+class Root(object):
+    # def GetData():
+        print "\n\nHello from Server!\n"
+    #     return "A message from the server!"
+
+
+def get_cp_config():
+    return {
+        '/': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': PATH,
+            'tools.staticdir.index': 'index.html',
+        },
+    }
+
+
+def runserver(config):
+    cherrypy.tree.mount(Root(), '/', config)
+    cherrypy.server.socket_host = "0.0.0.0"
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
+
+if __name__ == "__main__":
+    runserver(get_cp_config())
+else:
+    cherrypy.config.update({'environment': 'embedded'})
+    application = cherrypy.Application(
+        Root(), script_name=None, config=get_cp_config())
