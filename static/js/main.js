22507b19c23a06902847a50165fc6c23cb11b1e1
Cyrille Gindreau <Cyrille@Cyrilles-MacBook-Pro.local>
2016-09-21 12:34:57 -0600

    header img

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,11 @@
 angular.module('lifeTreks.main', [])
 
 .controller('lifeTreksMain', function($scope){
+	$scope.contentpage = "/html/pages/homepagecontent.html";
+    $scope.menupage = "/html/pages/homepagemenu.html";
+
 	$scope.menuBtn = function(path) {
     	$scope.contentpage = "/html/pages/" + path + "content.html";
     	$scope.menupage = "/html/pages/" + path + "menu.html";
-    	console.log("here");
 	};
-
 });
