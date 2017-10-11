20af38be7b80fec40695b9d2c93c432c02f2241a
Cyrille Gindreau <cyrillegin@gmail.com>
2017-10-10 19:19:28 -0600

    creating contact submission to email behavior

--- src/client/pages/about/about.controller.js
+++ src/client/pages/about/about.controller.js
@@ -2,14 +2,34 @@ import './about-page.style.scss';
 
 export default class aboutController {
 
-    constructor($scope) {
+    constructor($scope, $http) {
         'ngInject';
         this.$scope = $scope;
 
+        $('#form-submit').on('click', (event) => {
+            const message = {
+                title: $('#form-title').val(),
+                body: $('#form-body').val(),
+                email: $('#form-from-email').val()
+            };
+            const req = {
+                method: 'POST',
+                url: '/',
+                data: message,
+            };
+            $http.post('/message', message).then((success) => {
+                console.log(success);
+                console.log('good')
+            })
+            .catch((error) => {
+                console.log('got error')
+                console.log(error)
+            })
+
+        });
+
     }
     $onInit() {
-        $('#form-submit').on('click', (event) => {
-            console.log('got a hit');
-        });
+
     }
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,11 @@
 import path from 'path';
 import express from 'express';
 
+
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv').config()
+
 const app = express();
 const port = process.env.PORT || '5000';
 
@@ -11,4 +16,43 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(process.env.PWD, 'dist/server/public/index.html'));
 });
 
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.post('/message', (req, res) => {
+    sendEmail(req.body);
+});
+
+
+function sendEmail(emailContent) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS,
+        },
+    });
+
+    let message = 'That was easy!';
+    for (const index in emailContent) {
+        message += emailContent[index];
+    }
+
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        subject: 'Sending Email using Node.js',
+        text: message,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(`Email sent: ${ info.response}`);
+        }
+    });
+}
 app.listen(port);
