ed7fecc5f9537b8d82552cf9180c22da59eb3f8d
Cyrille Gindreau <cyrillegin@gmail.com>
2017-10-19 11:35:36 -0600

    added social media icons to navbar

--- src/client/components/navbar/navbar.component.js
+++ src/client/components/navbar/navbar.component.js
@@ -1,6 +1,7 @@
 // import navController from './navbar.controller';
 import nav from './navbar.html';
 import './navbar.style.scss';
+import 'font-awesome/css/font-awesome.min.css';
 
 const navcomponent = {
     template: nav,
