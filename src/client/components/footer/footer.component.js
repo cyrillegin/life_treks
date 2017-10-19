6d7969f4970bacfdde5e0d9729e2252ed91864d2
Cyrille Gindreau <cyrillegin@gmail.com>
2017-10-19 12:15:30 -0600

    footer

---
+++ src/client/components/footer/footer.component.js
@@ -0,0 +1,11 @@
+// import navController from './navbar.controller';
+import foot from './footer.html';
+import './footer.style.scss';
+import 'font-awesome/css/font-awesome.min.css';
+
+const navcomponent = {
+    template: foot,
+    // controller: navController,
+};
+
+export default navcomponent;
