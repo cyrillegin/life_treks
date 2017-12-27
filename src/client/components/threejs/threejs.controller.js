abc685e7e80e499d1a2822851805d72fd518f6c6
Cyrille Gindreau <cyrillegin@gmail.com>
2017-12-27 16:59:22 -0700

    updating three libraries to use obj importing

--- src/client/components/threejs/threejs.controller.js
+++ src/client/components/threejs/threejs.controller.js
@@ -1,8 +1,10 @@
-import * as THREE from 'three';
+import 'three';
+import 'three/examples/js/loaders/OBJLoader';
 
 export default class threejs {
 
     constructor($scope) {
+      console.log(THREE)
         const container = document.getElementById('renderer');
         const app = this.doThree();
         app.init(container);
@@ -30,46 +32,46 @@ export default class threejs {
                     initCamera();
                     initLights();
                 }
-                // Create Renderer
                 function initRenderer() {
                     app.renderer = new THREE.WebGLRenderer({
                         antialias: true,
-                        // alpha: true,
+                        alpha: true,
                     });
-                    // app.renderer.shadowMap.enabled = true;
-                    // app.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+                    app.renderer.shadowMap.enabled = true;
+                    app.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
                     app.renderer.gammaInput = true;
                     app.renderer.gammaOutput = true;
                     app.renderer.autoClear = false;
                     app.renderer.sortObjects = false;
-                    app.renderer.setSize(600, 300);
-                    app.renderer.setClearColor(0x000000, 0);
+                    app.renderer.setSize(container.offsetWidth, container.offsetHeight);
+
+                    app.renderer.setClearColor(0xffffff, 0);
                     container.appendChild(app.renderer.domElement);
                     $(app.renderer.domElement).css('position', 'absolute');
                 }
 
                 // Create lights
                 function initLights() {
-                    app.sun = new THREE.SpotLight(0xffffff, 100);
-                    app.sun.position.set(-6, 6, 6);
+                    app.sun = new THREE.SpotLight(0xffffff, 1);
+                    app.sun.position.set(-60, 60, 60);
                     app.sun.angle = 1.1;
                     app.sun.decay = 0;
-                    // app.sun.castShadow = true;
-                    //
-                    // app.sun.shadow.mapSize.width = 2048;
-                    // app.sun.shadow.mapSize.height = 2048;
-                    // app.sun.shadow.camera.near = 1;
-                    // app.sun.shadow.camera.far = 80000;
+                    app.sun.castShadow = true;
+
+                    app.sun.shadow.mapSize.width = 2048;
+                    app.sun.shadow.mapSize.height = 2048;
+                    app.sun.shadow.camera.near = 1;
+                    app.sun.shadow.camera.far = 80000;
                     app.scene.add(app.sun);
-                    backlight = new THREE.AmbientLight(0x00ffff, 10.3);
+                    backlight = new THREE.AmbientLight(0xffffff, 0.3);
                     backlight.position.set(10, -10, -10);
                     app.scene.add(backlight);
                 }
 
                 // Create Camera and attach OrbitControls.js
                 function initCamera() {
-                    app.camera = new THREE.PerspectiveCamera(45, 600 / 300, 2, 80000);
-                    app.camera.position.set(10, 5, 0);
+                    app.camera = new THREE.PerspectiveCamera(45, container.offsetWidth / container.offsetHeight, 2, 80000);
+                    app.camera.position.set(25, 15, 0);
                     app.camera.lookAt(app.scene.position);
                     // app.cameraControls = new THREE.OrbitControls(app.camera, app.renderer.domElement);
                 }
@@ -80,19 +82,35 @@ export default class threejs {
                         app.scene.remove(app.displayedMesh);
                     }
                     if (app.displayedMesh === null) {
-                        const loader = new THREE.JSONLoader();
-                        loader.load(('models/arduino.json'), (geometry, materials) => {
-                            const mesh = new THREE.Mesh(geometry, materials);
-                            mesh.doubleSided = true;
-                            // mesh.castShadow = true;
-                            // mesh.receiveShadow = true;
-                            mesh.position.set(0, 0, 0);
-                            mesh.name = name;
-                            app.meshes[name] = mesh;
-                            app.scene.add(mesh);
-                            app.displayedMesh = mesh;
-                            app.loaded = true;
-                        });
+                        // const loader = new THREE.JSONLoader();
+                        // loader.load(('models/test_syn.json'), (geometry, materials) => {
+                        //     const mesh = new THREE.Mesh(geometry, materials);
+                        //     mesh.doubleSided = true;
+                        //     // mesh.castShadow = true;
+                        //     // mesh.receiveShadow = true;
+                        //     mesh.position.set(0, 0, 0);
+                        //     mesh.name = name;
+                        //     app.meshes[name] = mesh;
+                        //     app.scene.add(mesh);
+                        //     app.displayedMesh = mesh;
+                        //     app.loaded = true;
+                        // });
+
+                        const objLoader = new THREE.OBJLoader();
+                        objLoader.load(
+                            'models/test.obj',
+                            (object) => {
+                                app.scene.add(object);
+                            },
+                            // called when loading is in progresses
+                          	(xhr) => {
+                        		    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+                            },
+                          	// called when loading has errors
+                          	(error) => {
+                        		console.log('An error happened');
+                        	},
+                        );
                     } else {
                         app.scene.add(app.meshes[name]);
                         app.displayedMesh = app.meshes[name];
