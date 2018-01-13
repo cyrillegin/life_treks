1beaf2c6fcf5cc97752c39ef5c5c95a21708f7d0
Cyrille Gindreau <cyrillegin@gmail.com>
2018-01-13 15:29:32 -0700

    creating nav bar.

--- src/client/pages/blog-roll/blog.controller.js
+++ src/client/pages/blog-roll/blog.controller.js
@@ -1,9 +1,10 @@
 export default class blogController {
 
-    constructor($scope, $http) {
+    constructor($scope, $http, $location) {
         'ngInject';
         this.$scope = $scope;
         this.$http = $http;
+        this.$location = $location;
     }
 
     $onInit() {
@@ -12,10 +13,9 @@ export default class blogController {
     }
 
     loadBlogs(count) {
-        this.$http.get('/blogs')
+        console.log(this.$location.url())
+        this.$http.get(`/blogs${this.$location.url()}`)
             .then((success) => {
-                console.log('got success');
-                console.log(success);
                 this.$scope.blogs = success.data;
             })
             .catch((error) => {
