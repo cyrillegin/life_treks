c117685730eabc310138573f6e3b92dc805831fd
Cyrille Gindreau <cyrillegin@gmail.com>
2018-09-09 12:55:10 -0600

    google analytics

--- src/client/routes/App.js
+++ src/client/routes/App.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import ReactGA from 'react-ga';
 import BoatContainer from './../pages/Boat/BoatContainer';
 import DragonflyContainer from './../pages/Dragonfly/DragonflyContainer';
 import VisualizationContainer from './../pages/Visualization/VisualizationContainer';
 // Nav
 import NavBar from './../components/NavBar/NavBar';
 
+ReactGA.initialize('UA-107911028-1');
+
 export default class App extends Component {
   static propTypes = {};
+
+  fireTracking() {
+    ReactGA.pageview(window.location.hash);
+  }
 
   componentDidMount() {
     // hide the preloader.
@@ -16,7 +23,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router onUpdate={this.fireTracking}>
         <div>
           <NavBar />
           <Route exact path="/boat" component={BoatContainer} />
